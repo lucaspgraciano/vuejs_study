819e95203e43d55dc341d438cda526618d3f1d0f
lucaspgraciano <lucaspgraciano@hotmail.com>
2021-02-10 04:24:35 -0300

    filtros e mixin

---
+++ Filtros-mixins/filtros-mixins-desafio/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins (Desafio)</h1>
+		<p>{{  frase | espacoVirgula}}</p>
+		
+		<p>{{ frase | contaPalavras}}</p>
+
+		<p>{{ fraseComVirgulas }}</p>
+		<p>{{ fraseComTamanhos }}</p>
+
+		<!-- Exercício 4 -->
+		<!-- Compartilhe a propriedade computada via mixin -->
+	</div>
+</template>
+	
+<script>
+import MixIn from './mixIn'
+export default {
+	mixins: [MixIn],
+	filters: {
+		espacoVirgula(valor) {
+			return valor.replace(/\s/g, ',')
+		}
+	},
+	data() {
+		return{
+			frase: 'Frase de teste do desafio.'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
---
+++ Filtros-mixins/filtros-mixins-exercicios/src/components/frutas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">{{fruta}}</li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+</template>
+
+<script>
+import frutasMixin from './frutasMixin'
+export default {
+    mixins: [frutasMixin]
+}
+</script>
+
+<style>
+    li{
+        list-style: none;
+    }
+</style>

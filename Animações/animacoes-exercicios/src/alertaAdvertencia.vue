ca63b580f701f5e0cc49d1072be06857ae1866a6
lucaspgraciano <lucaspgraciano@hotmail.com>
2021-02-10 11:17:25 -0300

    animações

---
+++ Animações/animacoes-exercicios/src/alertaAdvertencia.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-alert variant="warning" show>UMA MENSAGEM IMPORTANTE!</b-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Animações/animacoes-exercicios/src/alertaInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-alert variant="info" show>UMA MENSAGEM IMPORTANTE!</b-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

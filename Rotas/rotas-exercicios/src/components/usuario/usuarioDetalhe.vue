cc998f1268e18d2880bed988666341fe447566af
lucaspgraciano <lucaspgraciano@hotmail.com>
2021-02-12 10:52:32 -0300

    rotas com VueRouter

---
+++ Rotas/rotas-exercicios/src/components/usuario/usuarioDetalhe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="usuario-detalhe">
+      <h3>Usuário Detalhe</h3>
+      <hr>
+      <p><strong>Código:</strong> {{ id }}</p>
+      <router-link tag="button" primario 
+        :to="{ name: 'editarUsuario', params: {id}, query: {completo: true, lingua: 'pt-br'}, hash: '#rodape'}">Editar</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    beforeRouteEnter(to, from, next) {
+        const autenticar = true
+        autenticar ? next() : next(false)
+    } 
+    // data() {
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     $route(to) {
+    //         this.id = to.params.id
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
